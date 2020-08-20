b762ab3e899d76a8f8ffacdeabad0b512fa09e72
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-20 22:34:04 +0100

    yeet

--- src/algorithms/mergeSort.js
+++ src/algorithms/mergeSort.js
@@ -1,3 +1,14 @@
-const mergeSortAnimation = () => {};
+const mergeSortComplete = (array) => {
+	const animation = [];
+	if (array.length <= 1) return array;
+	const copyArray = array.slice();
+};
 
-export default mergeSortAnimation;
+const merge = (arr, start, mid, end) => {};
+
+const mergeSort = (arr, start, end) => {
+	if (start === end) return;
+	const mid = Math.floor((start + end) / 2);
+};
+
+export default mergeSortComplete;
