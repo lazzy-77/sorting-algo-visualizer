1191fea84e0cb5c76fbe184fdf6e11a05bdc1eb5
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-20 22:58:40 +0100

    basic setup

--- src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
+++ src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import moduleName from './';
+
+export default class SortingAlgoVisualizer extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			array: [],
+		};
+	}
+}
