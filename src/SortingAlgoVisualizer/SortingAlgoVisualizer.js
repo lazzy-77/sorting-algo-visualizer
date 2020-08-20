7db611e3872c7ca11833b3255e485248cbccfbba
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-21 00:51:40 +0100

    added more functionality

--- src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
+++ src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
@@ -1,36 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SortingAlgoVisualizer.css';
 
 const SortingAlgoVisualizer = (props) => {
 	const [count, setCount] = useState(0);
 	const [array, setArray] = useState([]);
+	//const [arraySize, setArraySize] = useState(0)
+
+	const arraySize = 450;
 
 	useEffect(() => {
 		resetArray();
-		return () => {
-			cleanup;
-		};
-	}, [input]);
+		return () => {};
+	}, []);
 
 	const resetArray = () => {
 		const array = [];
-		for (let i = 0; i < 100; i++) {
-			array.push(randomIntGenerator(5, 1000));
+		for (let i = 0; i < arraySize; i++) {
+			array.push(randomIntGenerator(5, 800));
 		}
 		setArray(array);
 	};
 
 	return (
-		<>
+		<div className='array-container'>
 			{array.map((value, id) => (
-				<div className='array-bar' key={id}>
-					{value}
-				</div>
+				<div
+					className='array-bar'
+					key={id}
+					style={{ height: `${value}px` }}
+				></div>
 			))}
-		</>
+			<button onClick={() => resetArray()}>Generate New Array</button>
+		</div>
 	);
 };
 
-const randomIntGenerator = (min, max) => {};
+const randomIntGenerator = (min, max) => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 export default SortingAlgoVisualizer;
