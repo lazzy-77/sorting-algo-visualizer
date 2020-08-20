dc08355a06bf5df837ff445491f83450bdf3ff2d
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-20 23:28:56 +0100

    concept functionality added

--- src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
+++ src/SortingAlgoVisualizer/SortingAlgoVisualizer.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import moduleName from './';
+import React, { useState } from 'react';
+import './SortingAlgoVisualizer.css';
 
-export default class SortingAlgoVisualizer extends React.Component {
-	constructor(props) {
-		super(props);
+const SortingAlgoVisualizer = (props) => {
+	const [count, setCount] = useState(0);
+	const [array, setArray] = useState([]);
 
-		this.state = {
-			array: [],
+	useEffect(() => {
+		resetArray();
+		return () => {
+			cleanup;
 		};
-	}
-}
+	}, [input]);
+
+	const resetArray = () => {
+		const array = [];
+		for (let i = 0; i < 100; i++) {
+			array.push(randomIntGenerator(5, 1000));
+		}
+		setArray(array);
+	};
+
+	return (
+		<>
+			{array.map((value, id) => (
+				<div className='array-bar' key={id}>
+					{value}
+				</div>
+			))}
+		</>
+	);
+};
+
+const randomIntGenerator = (min, max) => {};
+
+export default SortingAlgoVisualizer;
