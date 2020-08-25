f437624fb9ff650f21afbac69771ffc72e429bc6
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-25 21:29:44 +0100

    Updates

---
+++ precache-manifest.f92ef82cb3906a192dbf88e6b105343b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae09c0ebca8acb526e3946f391ff1d94",
+    "url": "/sorting-algo-visualizer/index.html"
+  },
+  {
+    "revision": "73f66510e0e7d1754fae",
+    "url": "/sorting-algo-visualizer/static/css/main.2ad3e79a.chunk.css"
+  },
+  {
+    "revision": "3cefbe20440fd0725a6d",
+    "url": "/sorting-algo-visualizer/static/js/2.3ddc59e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting-algo-visualizer/static/js/2.3ddc59e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f66510e0e7d1754fae",
+    "url": "/sorting-algo-visualizer/static/js/main.10c2eff5.chunk.js"
+  },
+  {
+    "revision": "48a432e894576015f915",
+    "url": "/sorting-algo-visualizer/static/js/runtime-main.9205acb2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-algo-visualizer/precache-manifest.261cbccd487f114505bc53da733cbbd3.js"
+  "/sorting-algo-visualizer/precache-manifest.f92ef82cb3906a192dbf88e6b105343b.js"
 );
 
 self.addEventListener('message', (event) => {
